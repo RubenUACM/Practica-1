9efcf0dc648f04bd98f9518f92cfc37c9189af3b
Ruben Salazar <ruben.salazar@estudiantxs.uacm.edu.mx>
2025-09-04 20:50:14 -0600

    codigo2.c

--- codigo2.c
+++ codigo2.c
@@ -1,3 +1,8 @@
+/*
+ * Archivo: codigo2.c
+ * Descripcion: Este programa calcula una aproximacion del valor de Pi
+ * utilizando la serie de Leibniz.
+ */
 #include <stdio.h>
 
 int main() {
@@ -5,8 +10,10 @@ int main() {
     long num_iterations = 10000; // numero de iteraciones
     double denominator = 1.0;
     int sign = 1; // 1 para suma, -1 para resta
-
+// Bucle para iterar a traves de la serie de Leibniz.
     for (long i = 0; i < num_iterations; i++) {
+        // La formula de Leibniz es (4/1) - (4/3) + (4/5) - (4/7) ...
+        // Este paso calcula cada termino de la serie y lo agrega a 'pi'.
         pi += sign * (4.0 / denominator);
         denominator += 2.0;
         sign *= -1;
